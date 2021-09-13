ebe027d00694a7d26584ba14dc10a81e812cb211
Agostina <arivasforestello@udesa.edu.ar>
2021-09-13 15:34:30 -0300

    footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+function Footer(){
+    return(
+        <React.Fragment>
+            <footer className='footer bg-dark'>
+                <div>
+                    <p className='m-0 text-center text-white'>Agostina Rivas</p>
+                    <p className='m-0 text-center text-white'>Mariana Telechea</p>
+                </div>
+            </footer>
+        </React.Fragment>
+    )
+}
+export default Footer;
