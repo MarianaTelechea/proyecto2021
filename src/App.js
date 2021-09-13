7ae474bd330e41bc7b9943819d004b766d9574c1
Agostina <arivasforestello@udesa.edu.ar>
2021-09-13 15:34:15 -0300

    componente app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import PeliculasPopu from './components/PeliculasPopu/PeliculasPopu';
 import PeliculasTop from './components/PeliculasTop/PeliculasTop';
+import PeliculasNP from './components/PeliculasNP/PeliculasNP';
+import PeliculasUp from './components/PeliculasUp/PeliculasUp';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -25,6 +28,26 @@ function App() {
             <PeliculasTop />
         </div>
       </div>
+
+      <div className='py-5'>
+        <div className='container px-4 px-lg-5 mt-5'>
+            <div className='title_catego'>
+              <h3> Películas en cartelera </h3>
+            </div>
+            <PeliculasNP />
+        </div>
+      </div>
+      <div className='py-5'>
+        <div className='container px-4 px-lg-5 mt-5'>
+            <div className='title_catego'>
+              <h3> Películas a estrenarse </h3>
+            </div>
+            <PeliculasUp />
+        </div>
+      </div>
+      <footer className='py-5 bg-dark'>
+        <Footer />
+      </footer> 
 
     </React.Fragment>
   );
