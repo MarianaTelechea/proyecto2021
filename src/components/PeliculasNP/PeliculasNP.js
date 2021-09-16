b034124b32823b8ab4a5c3f00b28eac932cd9f24
Agostina <arivasforestello@udesa.edu.ar>
2021-09-16 18:21:06 -0300

    Modificaciones componentes padres

--- src/components/PeliculasNP/PeliculasNP.js
+++ src/components/PeliculasNP/PeliculasNP.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react';
 import './PeliculasNP.css';
 import CardNP from '../CardNP/CardNP'
-// import FilterField from '../FilterField/FilterField' # EL FUTURO BUSCADOR
+import FilterField from '../FilterField/FilterField'
 class PeliculasNP extends Component{
     constructor(){
         super();
         this.state = {
             peliculas:[],
             peliculasIniciales: [],
-            nextUrl : ''
+            nextUrl : '',
+            orientacion: 'column'
         }
     }
     componentDidMount(){
@@ -36,7 +37,7 @@ class PeliculasNP extends Component{
         })
         .then((data) =>{
             this.setState({
-                peliculas : this.state.peliculas.concat(data.results),
+                peliculas : this.state.peliculas.concat(data.results.slice(0,10)),
                 nextUrl : 'https://api.themoviedb.org/3/movie/top_rated?api_key=65eadee9d6749b2ab92f01099d10deeb&language=en-US&page=2'
             })
         })
@@ -46,7 +47,23 @@ class PeliculasNP extends Component{
             return pelicula.id != id
         })
         this.setState({
-            peliculas : peliculasQuedan
+            peliculas : peliculasQuedan //es un estado
+        })
+    }
+    filtrarPeliculas(textoAFiltrar){ 
+        let peliculasFiltradas = this.state.peliculasIniciales.filter(pelicula=>{  // me estoy trayendo todos los datos que yo tengo adentro de ese estado. 
+            return pelicula.original_title.toLowerCase().includes(textoAFiltrar.toLowerCase()) 
+            //tolowecase():agarrar lo que se tiene atrapado y lo pone en minuscula
+            //includes() verifica si realmente el dato q me esta llegado realmente existe o no
+        }) 
+        this.setState({ //actualizo los personajes
+            peliculas : peliculasFiltradas
+        }) 
+        // Todos esto lo tengo que pasar al compenente hijo (filterField), quien va a llamar al metodo
+    }
+    cambiarOrientacion(orientacion){
+        this.setState({
+            orientacion: orientacion
         })
     }
     render(){
@@ -54,20 +71,31 @@ class PeliculasNP extends Component{
         console.log(this.state.peliculas);
         return(
             <React.Fragment>
-                {/* <div>
-                    <FilterField filtrarPersonajes = { (texto) => this.filtrarPersonajes(texto) }/>
-                </div> */}
-                <div className='row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center'>
-                    {
+                <div className='navbusc'>
+                    <FilterField filtrarPeliculas = { (texto) => this.filtrarPeliculas(texto) }/>
+                    <div className='imagenesnav'>
+                        <img onClick={()=>this.cambiarOrientacion('column')} className='vista matriz' src="/assets/images/matriz.png" alt=""/>
+                        <img onClick={()=>this.cambiarOrientacion('row')} className='vista lista' src="/assets/images/lista.png" alt=""/>
+                    </div>
+                </div>
+                <div className={`${this.state.orientacion == 'column' ? 
+                    'row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center' : 
+                    'row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center lista'
+                }`}
+                >
+                                
+                    { 
                         //Con este if ternario controlo por si tarda la carga de datos me aparezca un mensaje que dice cargando aplicación
                         this.state.peliculas.length === 0 ?
                         <div className='cargando'><h4>Cargando aplicación...</h4></div> :
                         this.state.peliculas.map((pelicula, index)=><CardNP key={pelicula.original_title + index} dataPelicula={pelicula}
                         //Aqui debemos pasarle el método (borrarTarjeta) al hijo
-                        borrar = {(idEliminar) => this.borrarTarjeta(idEliminar) }
-                        />)
+                        borrar = {(idEliminar) => this.borrarTarjeta(idEliminar) } // dentro de la propiedad borrar le pasamos al hijo, como si fuese una props més, el método de borrarTarjeta(). Dentro de este le pasamos unas props, donde le pasamos un método (idEliminar). Despues de la "=>" llamo al metodo que quiero pasarle al hijo con el this.borrarTajeta(). El idEliminar es el partametro que tiene que mandar el hijo al padre para poder eliminar la tarjeta
+                        />) 
                     }
+                    
                 </div>
+                
                 <div className="mas-pelis">
                 <button className='btn btn-outline-dark mt-auto' onClick= {() => this.masPeliculas() } >+</button>
                 </div>
