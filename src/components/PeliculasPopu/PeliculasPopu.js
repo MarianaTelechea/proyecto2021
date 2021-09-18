5cd06a13e96fd7b651e06a3e2cb629e4acc1f10b
Mariana <mtelechea@udesa.edu.ar>
2021-09-18 17:35:40 -0300

    Arreglos codigos

--- src/components/PeliculasPopu/PeliculasPopu.js
+++ src/components/PeliculasPopu/PeliculasPopu.js
@@ -10,9 +10,11 @@ class PeliculasPopu extends Component{
             peliculas:[],
             peliculasIniciales: [],
             nextUrl : '',
-            orientacion: 'column'
+            orientacion: 'column',
+            render: []
         }
     }
+
     componentDidMount(){
         console.log("Se cargo el componente");
         let url = 'https://api.themoviedb.org/3/movie/popular?api_key=65eadee9d6749b2ab92f01099d10deeb&language=en-US&page=1';
@@ -26,7 +28,8 @@ class PeliculasPopu extends Component{
                 this.setState({
                     peliculas: data.results,
                     peliculasIniciales: data.results,
-                    nextUrl: 'https://api.themoviedb.org/3/movie/popular?api_key=65eadee9d6749b2ab92f01099d10deeb&language=en-US&page=2'
+                    nextUrl: 'https://api.themoviedb.org/3/movie/popular?api_key=65eadee9d6749b2ab92f01099d10deeb&language=en-US&page=2',
+                    render: false //Set render state to false
                 })
             })
             .catch(error => console.log(error))
@@ -49,7 +52,7 @@ class PeliculasPopu extends Component{
 
     borrarTarjeta(id){
         let peliculasQuedan = this.state.peliculas.filter(pelicula =>{
-            return pelicula.id != id
+            return pelicula.id !== id
         })
         this.setState({
             peliculas : peliculasQuedan //es un estado
@@ -63,8 +66,12 @@ class PeliculasPopu extends Component{
             //includes() verifica si realmente el dato q me esta llegado realmente existe o no
         }) 
         this.setState({ //actualizo los personajes
-            peliculas : peliculasFiltradas
+            peliculas : peliculasFiltradas,
+            render: false
         }) 
+        setTimeout(function() { //Start the timer
+            this.setState({render: true}) //After 1 second, set render to true
+        }.bind(this), 1000)
 
         // Todos esto lo tengo que pasar al compenente hijo (filterField), quien va a llamar al metodo
     }
@@ -88,27 +95,40 @@ class PeliculasPopu extends Component{
                         <img onClick={()=>this.cambiarOrientacion('row')} className='vista lista' src="/assets/images/lista.png" alt=""/>
                     </div>
                 </div>
-                <div className={`${this.state.orientacion == 'column' ? 
+                <div className={`${this.state.orientacion === 'column' ? 
                     'row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center' : 
                     'row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center lista'
-                }`}
-                >
+                }`}>
                                 
                     { 
                         //Con este if ternario controlo por si tarda la carga de datos me aparezca un mensaje que dice cargando aplicación
                         this.state.peliculas.length === 0 ?
-                        <div className='cargando'><h4>Cargando aplicación...</h4></div> :
+                        // <div className='cargando'><h4>Cargando aplicación...</h4></div> :
+                        <div className="div">
+                        {this.state.render ?
+                        <div class="alert alert-danger d-flex align-items-center" role="alert">
+                            <div> No hay resultados :(</div>
+                        </div>:
+                        <button class="spinner btn btn-primary" id="mydiv" type="button" disabled>
+                        <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                            Loading...
+                        </button>}
+                         </div>:
                         this.state.peliculas.map((pelicula, index)=><CardPopu key={pelicula.original_title + index} dataPelicula={pelicula}
                         //Aqui debemos pasarle el método (borrarTarjeta) al hijo
                         borrar = {(idEliminar) => this.borrarTarjeta(idEliminar) } // dentro de la propiedad borrar le pasamos al hijo, como si fuese una props més, el método de borrarTarjeta(). Dentro de este le pasamos unas props, donde le pasamos un método (idEliminar). Despues de la "=>" llamo al metodo que quiero pasarle al hijo con el this.borrarTajeta(). El idEliminar es el partametro que tiene que mandar el hijo al padre para poder eliminar la tarjeta
                         />) 
-                    }
+                    }  
+                </div>
+                {
+                    this.state.peliculas.length !== 0 ?
+                    <div className="mas-pelis">
+                    <button className='btn btn-outline-dark mt-auto' onClick= {() => this.masPeliculas()} >+</button>
+                    </div>:
+                    ''
+
                     
-                </div>
-                
-                <div className="mas-pelis">
-                <button className='btn btn-outline-dark mt-auto' onClick= {() => this.masPeliculas() } >+</button>
-                </div>
+                }
             </React.Fragment>
         )
     }
