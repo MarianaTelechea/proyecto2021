a4a43889abfd471ac20d1f70a465d0b70b6335df
Agostina <arivasforestello@udesa.edu.ar>
2021-09-18 17:30:46 -0300

    Arreglos

--- src/components/PeliculasNP/PeliculasNP.js
+++ src/components/PeliculasNP/PeliculasNP.js
@@ -9,7 +9,8 @@ class PeliculasNP extends Component{
             peliculas:[],
             peliculasIniciales: [],
             nextUrl : '',
-            orientacion: 'column'
+            orientacion: 'column',
+            render: ''
         }
     }
     componentDidMount(){
@@ -44,7 +45,7 @@ class PeliculasNP extends Component{
     }
     borrarTarjeta(id){
         let peliculasQuedan = this.state.peliculas.filter(pelicula =>{
-            return pelicula.id != id
+            return pelicula.id !== id
         })
         this.setState({
             peliculas : peliculasQuedan //es un estado
@@ -57,8 +58,12 @@ class PeliculasNP extends Component{
             //includes() verifica si realmente el dato q me esta llegado realmente existe o no
         }) 
         this.setState({ //actualizo los personajes
-            peliculas : peliculasFiltradas
+            peliculas : peliculasFiltradas,
+            render: false
         }) 
+        setTimeout(function() { //Start the timer
+            this.setState({render: true}) //After 1 second, set render to true
+        }.bind(this), 1000)
         // Todos esto lo tengo que pasar al compenente hijo (filterField), quien va a llamar al metodo
     }
     cambiarOrientacion(orientacion){
@@ -78,29 +83,46 @@ class PeliculasNP extends Component{
                         <img onClick={()=>this.cambiarOrientacion('row')} className='vista lista' src="/assets/images/lista.png" alt=""/>
                     </div>
                 </div>
-                <div className={`${this.state.orientacion == 'column' ? 
+                <div className={`${this.state.orientacion === 'column' ? 
                     'row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center' : 
                     'row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center lista'
-                }`}
-                >
+                }`}>
                                 
                     { 
                         //Con este if ternario controlo por si tarda la carga de datos me aparezca un mensaje que dice cargando aplicación
                         this.state.peliculas.length === 0 ?
-                        <div className='cargando'><h4>Cargando aplicación...</h4></div> :
+                        // <div className='cargando'><h4>Cargando aplicación...</h4></div> :
+                        <div className="div">
+                        {this.state.render ?
+                        <div class="alert alert-danger d-flex align-items-center" role="alert">
+                            <div> No hay resultados :(</div>
+                        </div>:
+                        <button class="spinner btn btn-primary" id="mydiv" type="button" disabled>
+                        <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                            Loading...
+                        </button>}
+                         </div>:
                         this.state.peliculas.map((pelicula, index)=><CardNP key={pelicula.original_title + index} dataPelicula={pelicula}
                         //Aqui debemos pasarle el método (borrarTarjeta) al hijo
                         borrar = {(idEliminar) => this.borrarTarjeta(idEliminar) } // dentro de la propiedad borrar le pasamos al hijo, como si fuese una props més, el método de borrarTarjeta(). Dentro de este le pasamos unas props, donde le pasamos un método (idEliminar). Despues de la "=>" llamo al metodo que quiero pasarle al hijo con el this.borrarTajeta(). El idEliminar es el partametro que tiene que mandar el hijo al padre para poder eliminar la tarjeta
                         />) 
-                    }
+                    }  
+                </div>
+                {
+                    this.state.peliculas.length !== 0 ?
+                    <div className="mas-pelis">
+                    <button className='btn btn-outline-dark mt-auto' onClick= {() => this.masPeliculas()} >+</button>
+                    </div>:
+                    ''
                     
-                </div>
-                
-                <div className="mas-pelis">
-                <button className='btn btn-outline-dark mt-auto' onClick= {() => this.masPeliculas() } >+</button>
-                </div>
+                }
             </React.Fragment>
         )
     }
 }
-export default PeliculasNP;
+
+export default PeliculasNP
+
+
+
+
